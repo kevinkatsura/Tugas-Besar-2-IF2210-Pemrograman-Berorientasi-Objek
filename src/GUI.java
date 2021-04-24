f7ba65e60fcf7d683619379b109a086594ac6f1e
Ardovigus <aureliusmarcelmc@gmail.com>
2021-04-24 17:17:55 +0700

    Update Desain

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class GUI {
-    public static void main(String[] args) {
+    public static int P_X = 0;
+    public static int P_Y = 0;
 
+    public static void main(String[] args) {
+        // ### AWAL INISIALISASI
         ArrayList<String> result = new ArrayList<>();
 
         try (FileReader f = new FileReader("./assets/map.txt")) {
@@ -30,15 +35,13 @@ public class GUI {
         String[][] result_char = new String[result.size()][result.get(0).length()];
 
         for (int i = 0; i < result.size(); i++) {
-            result_char[i] = result.get(i).split(" ");
+            result_char[i] = result.get(i).split("\t");
         }
 
         for (int i = 0; i < result_char.length; i++) {
             for (int j = 0; j < result_char[i].length; j++) {
-                //System.out.printf("%s", result_char[i][j]);
                 result_char[i][j] = result_char[i][j].replaceAll("[^a-zA-Z0-9]","");
             }
-            //System.out.printf("\n");
         }
 
         ImageIcon ground = new ImageIcon("./resource/ground.png");
@@ -46,48 +49,52 @@ public class GUI {
         ImageIcon mount = new ImageIcon("./resource/mount.png");
         ImageIcon sea = new ImageIcon("./resource/sea.png");
         ImageIcon invent_slot = new ImageIcon("./resource/inventory.png");
-        //ImageIcon player = new ImageIcon("./resource/player.png");
+        ImageIcon player = new ImageIcon("./resource/player.png");
+        ImageIcon title = new ImageIcon("./resource/title.png");
+        ImageIcon map = new ImageIcon("./resource/map.png");
+        ImageIcon inventory_text = new ImageIcon("./resource/inventory_text.png");
+        // ### AKHIR INISIALISASI
 
-        // FRAME
+        // ### AWAL FRAME
         JFrame window = new JFrame();
-        window.setSize(1500, 750);
+        window.setSize(1500, 725);
         window.setLayout(null);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Engimon's Bizzare Adventure");
+        // ### AKHIR FRAME
 
-        // BUTTON
-        JButton button_W = new JButton("W");
-        button_W.setBounds(120, 100, 50, 50);
-        window.add(button_W);
+        // ### AWAL TITLE
+        JLabel label_Title = new JLabel();
+        label_Title.setIcon(title);
+        label_Title.setBounds(1195,10,274,97);
+        window.add(label_Title);
+        // ### AKHIR TITLE
 
-        JButton button_S = new JButton("S");
-        button_S.setBounds(120, 160, 50, 50);
-        window.add(button_S);
+        // ### AWAL MAP
+        JPanel panel_Label_Map = new JPanel();
+        panel_Label_Map.setBounds(280,515,110,40);
+        window.add(panel_Label_Map);
 
-        JButton button_A = new JButton("A");
-        button_A.setBounds(60, 160, 50, 50);
-        window.add(button_A);
+        JLabel label_Map = new JLabel();
+        label_Map.setIcon(map);
+        panel_Label_Map.add(label_Map);
 
-        JButton button_D = new JButton("D");
-        button_D.setBounds(180, 160, 50, 50);
-        window.add(button_D);
-
-        JButton button_exec = new JButton("Execute Command");
-        button_exec.setBounds(40, 300, 200, 25);
-        window.add(button_exec);
-
-        // PANEL
         JLayeredPane panel_Map = new JLayeredPane();
-        panel_Map.setPreferredSize(new Dimension(900, 450));
+        panel_Map.setPreferredSize(new Dimension(26 * 64, 26 * 64));
         panel_Map.setLayout(null);
+
+        JLabel label_Player = new JLabel();
+        label_Player.setBounds(P_X * 64,P_Y * 64,64,64);
+        label_Player.setIcon(player);
+        panel_Map.add(label_Player,1);
 
         /////
 
-        //int counter = 0;
         for (int i = 0; i < result_char.length; i++) {
             for (int j = 0; j < result_char[i].length; j++) {
                 JLabel label_Tile = new JLabel();
-                label_Tile.setBounds(j * 32,i * 32,32,32);
+                label_Tile.setBounds(j * 64,i * 64,64,64);
 
                 if (result_char[i][j].equals("G")) {
                     label_Tile.setIcon(ground);
@@ -101,83 +108,136 @@ public class GUI {
                     System.out.printf("%s", result_char[i][j]);
                 }
 
-                panel_Map.add(label_Tile);
-                //counter++;
+                panel_Map.add(label_Tile, -1);
             }
         }
 
         /////
 
+        JButton button_W = new JButton("W");
+        button_W.setBounds(690, 540, 50, 50);
+        button_W.setActionCommand("comm_W");
+        button_W.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                P_Y--;
+                label_Player.setLocation(P_X * 64,P_Y * 64);
+            }
+        });
+        window.add(button_W);
+
+        JButton button_S = new JButton("S");
+        button_S.setBounds(690, 600, 50, 50);
+        button_S.setActionCommand("comm_S");
+        button_S.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                P_Y++;
+                label_Player.setLocation(P_X * 64,P_Y * 64);
+            }
+        });
+        window.add(button_S);
+
+        JButton button_A = new JButton("A");
+        button_A.setBounds(630, 600, 50, 50);
+        button_A.setActionCommand("comm_A");
+        button_A.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                P_X--;
+                label_Player.setLocation(P_X * 64,P_Y * 64);
+            }
+        });
+        window.add(button_A);
+
+        JButton button_D = new JButton("D");
+        button_D.setBounds(750, 600, 50, 50);
+        button_D.setActionCommand("comm_D");
+        button_D.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                P_X++;
+                label_Player.setLocation(P_X * 64,P_Y * 64);
+            }
+        });
+        window.add(button_D);
+
+        /////
+
         JScrollPane panel_Map_Scrollpane = new JScrollPane(panel_Map);
-        panel_Map_Scrollpane.setBounds(280,10,900,450);
+        panel_Map_Scrollpane.setBounds(280,10,900,500);
         panel_Map_Scrollpane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panel_Map_Scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         panel_Map_Scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         window.add(panel_Map_Scrollpane);
 
+        // ### AKHIR MAP
+
+        // ### AWAL EXEC
+        JButton button_exec = new JButton("Execute Command");
+        button_exec.setBounds(40, 500, 200, 25);
+        window.add(button_exec);
+
         JPanel panel_Info = new JPanel();
-        panel_Info.setBounds(280, 470, 900, 230);
-        panel_Info.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        window.add(panel_Info);
+//        panel_Info.setBounds(280, 470, 900, 230);
+//        panel_Info.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//        window.add(panel_Info);
+
+        JScrollPane panel_Info_Scrollpane = new JScrollPane(panel_Info);
+        panel_Info_Scrollpane.setBounds(15, 150, 250, 340);
+        panel_Info_Scrollpane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel_Info_Scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panel_Info_Scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(panel_Info_Scrollpane);
+
+        JTextArea ta_info = new JTextArea("");
+        ta_info.setLineWrap(true);
+        ta_info.setColumns(20);
+        ta_info.setRows(20);
+        panel_Info.add(ta_info);
+
+//        ta_info.setText("This is a sample text This is a sample text This is a sample text");
+//        ta_info.append("This is a sample text");
 
         JPanel panel_Input_Command = new JPanel();
-        panel_Input_Command.setBounds(40, 325, 200, 50);
+        panel_Input_Command.setBounds(40, 535, 200, 50);
         window.add(panel_Input_Command);
 
-        // TEXT FIELD
         JTextField text_field1 = new JTextField("", 17);
         panel_Input_Command.add(text_field1);
 
-        JLayeredPane panel_Inventory_Engimon = new JLayeredPane();
-        //panel_Inventory_Engimon.setBounds(1190,10,4 * 64, 5 * 64);
-        //panel_Inventory_Engimon.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel_Inventory_Engimon.setPreferredSize(new Dimension(4 * 64, 5 * 64));
-        panel_Inventory_Engimon.setLayout(null);
-        //window.add(panel_Inventory_Engimon);
+        // ### AKHIR EXEC
+
+        // ### AWAL INVENTORY
+        JPanel panel_Inventory_Text = new JPanel();
+        panel_Inventory_Text.setBounds(1195,150,250,40);
+        window.add(panel_Inventory_Text);
+
+        JLabel label_Inventory = new JLabel();
+        label_Inventory.setIcon(inventory_text);
+        panel_Inventory_Text.add(label_Inventory);
+
+        JLayeredPane panel_Inventory = new JLayeredPane();
+        //panel_Inventory.setBounds(1190,10,4 * 64, 5 * 64);
+        //panel_Inventory.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel_Inventory.setPreferredSize(new Dimension(4 * 64, 5 * 64));
+        panel_Inventory.setLayout(null);
+        //window.add(panel_Inventory);
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++) {
                 JLabel label_new = new JLabel();
                 label_new.setBounds(j * 64,i * 64,64,64);
                 label_new.setIcon(invent_slot);
-                panel_Inventory_Engimon.add(label_new);
+                panel_Inventory.add(label_new);
             }
         }
 
-        JScrollPane panel_Inventory_Engimon_Scrollpane = new JScrollPane(panel_Inventory_Engimon);
-        panel_Inventory_Engimon_Scrollpane.setBounds(1190,10,270,340);
-        panel_Inventory_Engimon_Scrollpane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel_Inventory_Engimon_Scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        panel_Inventory_Engimon_Scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        window.add(panel_Inventory_Engimon_Scrollpane);
-
-        JLayeredPane panel_Inventory_Skill = new JLayeredPane();
-        panel_Inventory_Skill.setPreferredSize(new Dimension(4 * 64, 5 * 64));
-        panel_Inventory_Skill.setLayout(null);
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 4; j++) {
-                JLabel label_new = new JLabel();
-                label_new.setBounds(j * 64, i * 64, 64, 64);
-                label_new.setIcon(invent_slot);
-                panel_Inventory_Skill.add(label_new);
-            }
-        }
-
-        JLabel label_z = new JLabel();
-        label_z.setBounds(0,0,32,32);
-        label_z.setIcon(ground);
-        panel_Inventory_Skill.add(label_z);
-
-        JScrollPane panel_Inventory_Skill_Scrollpane = new JScrollPane(panel_Inventory_Skill);
-        panel_Inventory_Skill_Scrollpane.setBounds(1190,360,270,340);
-        panel_Inventory_Skill_Scrollpane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel_Inventory_Skill_Scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        panel_Inventory_Skill_Scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        window.add(panel_Inventory_Skill_Scrollpane);
+        JScrollPane panel_Inventory_Scrollpane = new JScrollPane(panel_Inventory);
+        panel_Inventory_Scrollpane.setBounds(1195,200,270,340);
+        panel_Inventory_Scrollpane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel_Inventory_Scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panel_Inventory_Scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(panel_Inventory_Scrollpane);
+        // ### AKHIR INVENTORY
 
         window.show();
-
     }
 }
 
